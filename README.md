7b3bdaa19c0fdb1a782a5fc1eba9719486979243
kltzqu <kltz4074@gmail.com>
2025-07-11 00:32:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webcam Mod 
+
+Works for Minecraft version 1.21.4
+
+## Disclamer
+
+It shouldn't be used on public servers, only with people you trust. I think it's pretty easy to send a malicious payload and crash your game (or worse!). Use at your own risk.
+
+Also, this isn't production-ready. I made it for a YouTube video: https://youtu.be/pRvUX16k6yw
+
+## Features
+
+Adds your webcam's feed to your player and displays it in real time. It works great on LAN, and I tested it on an online server with two players (I don't know if more players are too much load for the server).
+
+This is a server/client mod, so it needs to be installed on both.
+
+![Screenshot of webcam](https://media.forgecdn.net/attachments/description/null/description_80a3c57d-7cbe-4cfe-8133-396c33945a52.png)
---
+++ src/client/java/com/lichcode/webcam/video/VideoCamara.java
@@ -0,0 +1,102 @@
+package com.lichcode.webcam.video;
+
+import com.lichcode.webcam.WebcamMod;
+import com.lichcode.webcam.Video.PlayerVideo;
+import com.github.sarxos.webcam.*;
+import org.jetbrains.annotations.Nullable;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class VideoCamara {
+    private static Webcam webcam;
+
+    public static void init() {
+        for(Webcam wc : Webcam.getWebcams()) {
+            try {
+                wc.open();
+                webcam = wc;
+                WebcamMod.LOGGER.info("Using webcam: {}", wc.getName());
+                return;
+            } catch (WebcamException e) {
+                WebcamMod.LOGGER.info("Webcam {} not usable, trying next one.", wc.getName());
+            }
+        }
+
+        throw new WebcamLockException("All webcams in use!");
+    }
+
+    public static void release() {
+        webcam.close();
+    }
+
+    public static List<String> getWebcamList() {
+        return Webcam.getWebcams().stream().map((wc) -> wc.getName()).toList();
+    }
+
+    public static void setWebcamByName(String name) {
+        Webcam wc = Webcam.getWebcamByName(name);
+        if (wc == null) {
+            throw new WebcamException("Webcam not found");
+        }
+        webcam.close();
+        try {
+            wc.open();
+        } catch (WebcamException e) {
+            throw e;
+        }
+
+        webcam = wc;
+    }
+
+    public static String getCurrentWebcam() {
+        if (webcam == null) {
+            return null;
+        }
+
+        return webcam.getName();
+    }
+
+    public static void get(PlayerVideo playerVideo) throws IOException {
+        BufferedImage image = webcam.getImage();
+        // Resize image to defined size
+        // TODO: maybe add x/y offset so user can define size
+        image = resize(image, playerVideo.width, playerVideo.height);
+
+        // Compress image using JPEG
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageOutputStream ios = ImageIO.createImageOutputStream(baos);
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpeg");
+        ImageWriter writer = writers.next();
+        JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+        jpegParams.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+        jpegParams.setCompressionQuality(1);
+        writer.setOutput(ios);
+        IIOImage outputImage = new IIOImage(image, null, null);
+
+        writer.write(null, outputImage, jpegParams);
+        writer.dispose();
+        ios.close();
+        // Save compressed image to object
+        playerVideo.setFrame(baos.toByteArray());
+    }
+
+    public static BufferedImage resize(BufferedImage original, int width, int height) {
+        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = resized.createGraphics();
+        g.drawImage(original, 0, 0, width, height, null);
+        g.dispose();
+
+        return resized;
+    }
+}
